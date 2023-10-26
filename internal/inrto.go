8349a9a1f9dbab9a45cbf89ed8a6aae0b1fa04de
Dikontay <dalimbayeva@icloud.com>
2023-10-27 00:16:25 +0600

    add strategy pattern

--- internal/inrto.go
+++ internal/inrto.go
@@ -1,11 +1,74 @@
 package internal
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+	"os"
+)
 
-func Start(){
+func Start() {
 	fmt.Println("HELLO PLEASE CHOOSE THE TYPE OF ANIMAL")
-	fmt.Println("1. ")
-	for{
+	fmt.Println("1. DOG 2. Owl 3. BEAR ")
+
+	var option string
+
+	_, err := fmt.Scan(&option)
+
+	handleErrors(err)
+	err = check(option)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	animalBehavior := createAnimal(option)
+	if animalBehavior == nil {
+		fmt.Println("Failed to create animal")
+		return
+	}
+
+	// Create an instance of Animal and set its behavior
+	animalStaregy := &AnimalContext{
+		behavior: animalBehavior,
+	}
+	animalStaregy.behavior.Print()
+	// for {
 		
+	// }
+
+	}
+
+
+
+func createAnimal(num string) AnimalStaregy {
+
+	var behavior AnimalStaregy
+
+	switch num {
+	case "1":
+		behavior = &Dog{Name: "dog"}
+	case "2":
+		behavior = &Owl{Name: "owl"}
+	case "3":
+		behavior = &Bear{Name: "bear"}
+	// Add cases for other animal types
+	default:
+		return nil
+	}
+
+	return behavior
+}
+
+func check(option string) error {
+	if option == "1" || option == "2" || option == "3" || option == "4" {
+		return nil
+	}
+
+	return errors.New("Incorrect option choosen")
+}
+
+func handleErrors(err error) {
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
 	}
 }
--- internal/models.go
+++ internal/models.go
@@ -1,12 +1,95 @@
 package internal
 
-type Animal struct{
-	Type interface{}
-	Name string
-	Mood string
-	Gigiena string
-	Starving string
+//STRATEGY
+
+type AnimalContext struct {
+	behavior AnimalStaregy
 }
+
+type AnimalStaregy interface {
+	Eat() string
+	BeDirty() string
+	Play() string
+	Print()
+}
+
+type Dog struct{
+	Name string
+
+}
+type Bear struct{
+	Name string
+
+}
+type Owl struct{
+	Name string
+}
+func (d *Bear) Eat() string {
+    return "++++++++++"
+}
+
+func (d *Bear) BeDirty() string {
+    return "++++++++++"
+}
+
+func (d *Bear) Play() string {
+    return "++++++++++"
+}
+
+func (d *Bear) Print() {
+	PrintAnimalPicture("bear")
+}
+
+func (d *Owl) Eat() string {
+    return "++++++++++"
+}
+
+func (d *Owl) BeDirty() string {
+    return "++++++++++"
+}
+
+func (d *Owl) Play() string {
+    return "++++++++++"
+}
+
+func (d *Owl) Print() {
+	PrintAnimalPicture("owl")
+}
+
+func (d *Dog) Eat() string {
+    return "++++++++++"
+}
+
+func (d *Dog) BeDirty() string {
+    return "++++++++++"
+}
+
+func (d *Dog) Play() string {
+    return "++++++++++"
+}
+
+func (d *Dog) Print() {
+	PrintAnimalPicture("dog")
+}
+
+type Cat struct{
+	Name string
+}
+
+func (c *Cat) Eat() string {
+    return "++++++++++"
+}
+
+func (c *Cat) BeDirty() string {
+    return "++++++++++"
+}
+
+func (c *Cat) Play() string {
+    return "++++++++++"
+}
+//////////////////////////////////////////////////////////////////
+
+
 
 
 
